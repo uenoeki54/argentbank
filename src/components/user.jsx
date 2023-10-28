476f6f7746367aa735536072812cb20b1dda065e
uenoeki54 <uenoeki54ban@gmail.com>
2023-10-28 16:17:25 +0200

    Le menu contextuel de changement d username est fonctionnel mais a peaufiner

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,18 +1,45 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useUpdateMutation } from '../slices/usersApiSlice';
+import { setUser } from '../slices/authSlice';
 
 function User() {
   const { userInfo } = useSelector((state) => state.auth);
   const { fetchInfo } = useSelector((state) => state.auth);
   const navigate = useNavigate();
+  const [usernom, setUsername] = useState('nonamed');
+  const dispatch = useDispatch();
+  const [edituser, { isLoading }] = useUpdateMutation();
+  const { token } = useSelector((state) => state.auth);
+  //  ON FAIT UN STATE POUR LE MENU CONTEXTUEL
+  const [open, setOpen] = useState('false');
+
   useEffect(() => {
     if (!userInfo) {
       toast.error('You are not logged in yet');
       navigate('/login');
     }
   }, [navigate]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(token);
+      const res = await edituser({ token, usernom }).unwrap();
+      //   ON APPELLE ICI AUSSI COMME DANS LA PAGE LOGIN LA FONCTION SETUSER POUR QUE LES INFOS PROFILES
+      //   SOIENT UPDATEES DANS LE STATE ET LE LOCALSTORAGE AVEC LE NOUVEAU USERNAME
+      dispatch(setUser({ ...res }));
+      toast.success(`new username set to:${res.body.userName}`);
+      console.log(res.message);
+      console.log(res.body.userName);
+    } catch (err) {
+      console.log('il ya une erreur non repertoriée');
+      toast.error(err?.data?.message || err?.error);
+    }
+  };
+
   return (
     <main>
       <div className="header">
@@ -22,10 +49,40 @@ function User() {
           {fetchInfo &&
             `${fetchInfo.body.firstName} ${fetchInfo.body.lastName} !`}
         </h1>
-        <Link className="edit-button" to="/edit">
+        <Link
+          className="edit-button"
+          onClick={() => {
+            setOpen(!open);
+          }}
+          {...open}
+        >
           Edit Name
         </Link>
       </div>
+
+      <section
+        className={open ? 'sign-in-content display' : ' sign-in-content hide'}
+      >
+        <form onSubmit={submitHandler}>
+          <div className="input-wrapper">
+            <label htmlFor="usernom">New Username</label>
+            <input
+              type="text"
+              id="usernom"
+              name="usernom"
+              value={usernom}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Enter your username"
+              required
+            />
+          </div>
+
+          <button className="sign-in-button">
+            {isLoading ? 'LOADING' : 'Choose'}
+          </button>
+        </form>
+      </section>
+
       <h2 className="sr-only">Accounts</h2>
       <section className="account">
         <div className="account-content-wrapper">
